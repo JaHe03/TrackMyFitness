1ec31706bf858661ee941e6764eb20518227c854
JaHe03 <jarynh14@gmail.com>
2025-03-03 15:38:42 -0500

    dir struc fix, django works, react works

--- backend/fitness/apps.py
+++ backend/fitness/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TmfConfig(AppConfig):
+class FitnessConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'TMF'
+    name = 'fitness'
