0d4112ea5f2e07ef874b6320dd0378b501d9f8e8
JaHe03 <jarynh14@gmail.com>
2025-03-04 14:15:06 -0500

    login url added

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from apps.accounts.views import CustomUserCreateView
+from apps.accounts.views import CustomUserCreateView, LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', CustomUserCreateView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
